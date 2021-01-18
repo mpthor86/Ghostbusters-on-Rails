2ebeeeb564d62b80445fe628e83b425b014d01e6
michael <mpthornton86@gmail.com>
2021-01-18 16:56:23 -0700

    security methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    include ApplicationHelper
+
+    private
+
+    def redirect_not_logged_in
+        redirect_to '/signin' if !logged_in?
+    end
 end
