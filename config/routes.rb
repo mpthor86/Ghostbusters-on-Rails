40c2ce2c4382922e979fc2b68592bf6cd58d7c80
michael <mpthornton86@gmail.com>
2021-01-18 17:00:19 -0700

    initial route setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'static#home'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  resources :ghosts
+  resources :appointments
+  resources :ghostbusters
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
