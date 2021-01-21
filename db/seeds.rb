122dd6662305ab0192b7b2ceb269107d1643805d
michael <mpthornton86@gmail.com>
2021-01-20 17:11:49 -0700

    DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ghostbuster.create([{name: 'Peter Venkman', years_experience: 36}, 
+                    {name: 'Ray Stantz', years_experience: 36}, 
+                    {name: 'Egon Spengler', years_experience: 36},
+                    {name: 'Winston Zeddmore', years_experience: 36}])
+
+User.create([{username: Faker::Name.name, password: 'password', address: Faker::Address.full_address},
+            {username: Faker::Name.name, password: 'password', address: Faker::Address.full_address},
+            {username: Faker::Name.name, password: 'password', address: Faker::Address.full_address}])
