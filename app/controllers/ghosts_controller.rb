ccc48f2daf1702483f443f4656340d16cc80d5c4
michael <mpthornton86@gmail.com>
2021-01-22 17:37:14 -0700

    setup ghost

--- app/controllers/ghosts_controller.rb
+++ app/controllers/ghosts_controller.rb
@@ -1,2 +1,28 @@
 class GhostsController < ApplicationController
+    def show
+        @ghost = Ghost.find(params[:id])
+    end
+
+    def index
+        if params[:user_id]
+            @ghost = current_user.ghosts
+        else
+        @ghost = Ghost.all
+        end
+    end
+
+    def edit
+        @ghost = Ghost.find(params[:id])
+    end
+
+    def destroy
+        Ghost.destroy(params[:id])
+        redirect_to user_ghosts_path(current_user)
+    end
+
+    private
+
+    def ghost_params
+        params.require(:ghost).permit(:user_id, :id)
+    end
 end
--- app/models/ghost.rb
+++ app/models/ghost.rb
@@ -1,5 +1,11 @@
 class Ghost < ApplicationRecord
     belongs_to :user
 
-    validates :name, presence: true, length: {maximum: 15}
+    validates :name, presence: true, length: {maximum: 15} 
+
+    def find_buster
+        appt = self.user.appointments.find{|a| a.created_at.strftime('%H:%M:%S') == self.created_at.strftime('%H:%M:%S')}
+        appt.ghostbuster.name
+    end
+    
 end
