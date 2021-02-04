55d01a0fdde474b96962903bced509cd9a98e627
michael <mpthornton86@gmail.com>
2021-02-04 14:03:59 -0700

    new routes for busters

--- app/controllers/ghostbusters_controller.rb
+++ app/controllers/ghostbusters_controller.rb
@@ -1,2 +1,9 @@
 class GhostbustersController < ApplicationController
+    def index
+        @busters = Ghostbuster.all
+    end
+
+    def show
+        @buster = Ghostbuster.find(params[:id])
+    end
 end
