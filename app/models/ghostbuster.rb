62da9b2a0565101825fa3fec85894fbdad3a2d59
michael <mpthornton86@gmail.com>
2021-02-04 14:04:44 -0700

    change scope method

--- app/models/ghostbuster.rb
+++ app/models/ghostbuster.rb
@@ -5,9 +5,8 @@ class Ghostbuster < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :years_experience, length: {minimum: 2}
 
-    def self.get_buster(time)
-        self.all.each do |b|
-            where("#{b.appointments.last.time} < ?", time)
-        end
+    def self.get_buster
+        #where("time > ?", time)
+        self.joins(:appointments).where.not("time > ?", Time.now)
     end
 end
